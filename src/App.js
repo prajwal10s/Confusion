38ebf357e9b9daa3254de27fe9ee24e04e8425ba
Yadavendra Sakharkar <prajwal1619@gmail.com>
2020-07-03 22:48:17 +0530

    Assignment 3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Main from "./components/MainComponent";
-import { BrowserRouter } from "react-router-dom";
+import { DISHES } from "./shared/dishes";
+
+import { Provider } from "react-redux";
+import { ConfigureStore } from "./redux/configureStore";
+
+const store = ConfigureStore();
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Main />
-        </div>
-      </BrowserRouter>
+      <div className="App">
+        <Provider store={store}>
+          <BrowserRouter>
+            <div className="App">
+              <Main />
+            </div>
+          </BrowserRouter>
+        </Provider>
+      </div>
     );
   }
 }
